4b59c8144436a4192c02db0f768b3547d1ed96f5
olekingkole28-dot <olekingkole28@gmail.com>
2026-09-12 13:28:51 -0400

    Advance Horizon cache to 4208

--- app/horizon/sw.js
+++ app/horizon/sw.js
@@ -1,4 +1,4 @@
-const CACHE='bridgepoint-horizon-test-v4203';
+const CACHE='bridgepoint-horizon-test-v4208';
 self.addEventListener('install',event=>event.waitUntil(self.skipWaiting()));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',event=>{if(event.request.method!=='GET')return;event.respondWith(fetch(event.request,{cache:'no-store'}).catch(()=>new Response('Horizon test world is temporarily unavailable',{status:503,headers:{'Content-Type':'text/plain'}}))) });
