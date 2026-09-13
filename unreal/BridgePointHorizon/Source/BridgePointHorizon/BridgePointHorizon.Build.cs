a8c38ce7742766d2c851a30375f72ddbefe97f8a
olekingkole28-dot <olekingkole28@gmail.com>
2026-09-12 23:35:48 -0400

    Build Horizon Unreal runtime foundation

---
+++ unreal/BridgePointHorizon/Source/BridgePointHorizon/BridgePointHorizon.Build.cs
@@ -0,0 +1,29 @@
+using UnrealBuildTool;
+
+public class BridgePointHorizon : ModuleRules
+{
+    public BridgePointHorizon(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+
+        PublicDependencyModuleNames.AddRange(new string[]
+        {
+            "Core",
+            "CoreUObject",
+            "Engine",
+            "InputCore",
+            "EnhancedInput",
+            "GameplayTags",
+            "GameplayAbilities",
+            "GameplayTasks",
+            "AIModule",
+            "NavigationSystem"
+        });
+
+        PrivateDependencyModuleNames.AddRange(new string[]
+        {
+            "Slate",
+            "SlateCore"
+        });
+    }
+}
