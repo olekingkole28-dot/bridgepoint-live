a3829232f465bec59cb63b88fb4057bacb4ddfa9
olekingkole28-dot <olekingkole28@gmail.com>
2026-09-13 14:04:44 -0400

    Horizon: refresh installed app shell cache

--- app/horizon/sw.js
+++ app/horizon/sw.js
@@ -1,4 +1,4 @@
-const CACHE='bridgepoint-horizon-install-v3';
+const CACHE='bridgepoint-horizon-install-v4';
 const SHELL=['./','./index.html','./lobby.html','./manifest.webmanifest','./horizon-mark.svg'];
 
 self.addEventListener('install',event=>{
