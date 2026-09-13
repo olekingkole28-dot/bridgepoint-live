f0ca63eca18684f88d40e423a8920a0e5e085961
olekingkole28-dot <olekingkole28@gmail.com>
2026-09-13 14:08:52 -0400

    Horizon: refresh installed shell for online lobby 4248

--- app/horizon/sw.js
+++ app/horizon/sw.js
@@ -1,4 +1,4 @@
-const CACHE='bridgepoint-horizon-install-v4';
+const CACHE='bridgepoint-horizon-install-v5';
 const SHELL=['./','./index.html','./lobby.html','./manifest.webmanifest','./horizon-mark.svg'];
 
 self.addEventListener('install',event=>{
