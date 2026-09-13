5e41c941f2b8499b63702c55913afe0dbe98f637
olekingkole28-dot <olekingkole28@gmail.com>
2026-09-13 18:24:32 -0400

    Horizon: refresh PWA shell for 4255

--- app/horizon/sw.js
+++ app/horizon/sw.js
@@ -1,4 +1,4 @@
-const CACHE='bridgepoint-horizon-install-v10';
+const CACHE='bridgepoint-horizon-install-v11';
 const SHELL=['./','./index.html','./lobby.html','./manifest.webmanifest','./horizon-mark.svg'];
 
 self.addEventListener('install',event=>{
