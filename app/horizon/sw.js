b4eda4a639994fb5c0792e5589e0ca98d44dcd33
olekingkole28-dot <olekingkole28@gmail.com>
2026-09-19 23:43:52 -0400

    Horizon PWA: bust cache for Year One V4336

--- app/horizon/sw.js
+++ app/horizon/sw.js
@@ -1,4 +1,4 @@
-const CACHE='bridgepoint-horizon-v4335';
+const CACHE='bridgepoint-horizon-v4336';
 const SHELL=[
   './','./index.html','./styles.css','./app.js','./runtime-contract.js','./lobby-scene.js','./model-preview.js',
   './manifest.webmanifest','./horizon-mark.svg'
